aaea01e852bf71be8b27d19996313375838f9c4e
paul-serdiuk <85490231+paul-serdiuk@users.noreply.github.com>
2021-06-24 20:50:47 +0300

    Support taking the password from the command line, use lightkdf parameters

--- generate.js
+++ generate.js
@@ -1,14 +1,20 @@
 // https://ethereum.stackexchange.com/a/11226/20417
 
 let privateKey = process.argv[2];
+let password = process.argv[3]
 
 if (!privateKey || privateKey.length === 0) {
   console.log("You need to specify a private key");
   process.exit(1);
 }
 
+if (!password || password.length === 0) {
+  console.log("You need to specify a password");
+  process.exit(1);
+}
+
 let Wallet = require('ethereumjs-wallet');
 let key = Buffer.from(privateKey, 'hex');
 let wallet = Wallet.fromPrivateKey(key);
-let s = wallet.toV3String('');
+let s = wallet.toV3String(password, {kdf: 'scrypt', dklen: 32, n: 4096, p: 6, r: 8, cipher: 'aes-128-ctr'});
 console.log(s);
